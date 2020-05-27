5a06b19dffb956a94f73e72176e48d7d2a6ca2b7
aaleksandrovg <aaleksandrov@mi9retail.com>
2020-05-27 11:17:04 +0300

    Updates

---
+++ precache-manifest.0bf089bc56503f2fd0486f82d79bffa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6ed432de1c11025b6e28107671ef2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be269f69103a6250245",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49f9d0c22f159eb8512b",
+    "url": "/static/js/2.11ccfc48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11ccfc48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be269f69103a6250245",
+    "url": "/static/js/main.595e5e66.chunk.js"
+  },
+  {
+    "revision": "846d8fb1817c14a13570",
+    "url": "/static/js/runtime-main.335ebb8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
